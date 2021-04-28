c87fe0044bae1f6e4b81fb4626df219d73b38afc
NguyenHieu <hieunguyenute@gmail.com>
2021-04-28 08:38:24 +0700

    create project

---
+++ AssessmentRound1TechBase/AssessmentRound1TechBaseTests/AssessmentRound1TechBaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  AssessmentRound1TechBaseTests.swift
+//  AssessmentRound1TechBaseTests
+//
+//  Created by Azibai on 28/04/2021.
+//  Copyright © 2021 Azibai. All rights reserved.
+//
+
+import XCTest
+@testable import AssessmentRound1TechBase
+
+class AssessmentRound1TechBaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
